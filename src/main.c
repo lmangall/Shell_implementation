ac52a4f2efa5ef08fb1ecf10b71a356e76a73cf3
Leonard <l.mangallon@gmail.com>
2023-08-19 20:29:21 +0200

    got rid of function read_cmd and print_prompt

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmangall <lmangall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 20:22:39 by lmangall          #+#    #+#             */
-/*   Updated: 2023/08/19 20:16:03 by lmangall         ###   ########.fr       */
+/*   Updated: 2023/08/19 20:29:03 by lmangall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,93 +19,21 @@
 #include "../include/minishell.h"
 #include "../lib/libft/src/libft.h"
 
-void print_prompt1(void)
-{
-    fprintf(stderr, "$ ");
-}
-void print_prompt2(void)
-{
-    fprintf(stderr, "> ");
-} 
-
 int main(int argc, char **argv)
 {
     char *cmd;
-
-    while(1)
-    {
-        //print_prompt1();
-        cmd = read_cmd();
-        if(!cmd)
-			exit(EXIT_SUCCESS);
-        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
-        {
-            free(cmd);
-            continue;// jump to the next iteration of the loop (skiping the rest). 
-        }
-        if(strcmp(cmd, "exit\n") == 0)
-        {
-            free(cmd);
-            break;
-        }
+        // if(strcmp(cmd, "exit\n") == 0)
+        // {
+        //     free(cmd);
+        //     break;
+        // }
         struct source_s src;
         src.buffer   = cmd;
         src.bufsize  = strlen(cmd);
         src.curpos   = INIT_SRC_POS;
         parse_and_execute(&src);        free(cmd);
-    } 
     exit(EXIT_SUCCESS);
 } 
-
-char *read_cmd(void)
-{
-    char buf[1024];
-    char *ptr = NULL;
-    char ptrlen = 0;
-
-    while(fgets(buf, 1024, stdin))
-    {
-        int buflen = strlen(buf);
-
-        if(!ptr)
-			ptr = malloc(buflen+1);
-        else
-        {
-            char *ptr2 = realloc(ptr, ptrlen+buflen+1);
-
-            if(ptr2)
-				ptr = ptr2;
-            else
-            {
-                free(ptr);
-                ptr = NULL;
-            }
-        }
-
-        if(!ptr)
-        {
-            fprintf(stderr, "error: failed to alloc buffer: %s\n", 
-                    strerror(errno));
-            return NULL;
-        }
-
-        strcpy(ptr+ptrlen, buf);
-
-        if(buf[buflen-1] == '\n')
-        {
-            if(buflen == 1 || buf[buflen-2] != '\\')
-				return ptr;
-
-            ptr[ptrlen+buflen-2] = '\0';
-            buflen -= 2;
-            print_prompt2();
-        }
-
-        ptrlen += buflen;
-    }
-
-    return ptr;
-}
 
 int parse_and_execute(struct source_s *src)
 {
