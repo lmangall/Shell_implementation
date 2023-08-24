3027ef984cb16c8c05018c0274f4fec8d1901a78
Leonard <l.mangallon@gmail.com>
2023-08-24 16:00:03 +0200

    expander draft commmented out

---
+++ include/expander.h
@@ -0,0 +1,30 @@
+#ifndef EXPANDER_H
+#define EXPANDER_H
+
+
+// /**
+//  * @brief Replaces the variable in the given node's string with its value.
+//  * 
+//  * @param node The node to replace the variable in.
+//  * @return int Returns 0 on success.
+//  */
+// static int replace_var(struct node_s *node);
+
+// /**
+//  * @brief Checks if the given node's string contains a dollar sign.
+//  * 
+//  * @param node The node to check.
+//  * @return struct node_s* Returns the node if it contains a dollar sign, otherwise returns NULL.
+//  */
+// struct node_s *contains_dollar(struct node_s *node);
+
+// /**
+//  * @brief Expands variables in the given node's string using the given variables container.
+//  * 
+//  * @param node The node to expand variables in.
+//  * @param vars_container The container of variables to use for expansion.
+//  */
+// void expansion(struct node_s *node, struct Vars *vars_container);
+
+
+#endif 
--- src/expander.c
+++ src/expander.c
@@ -6,7 +6,7 @@
 /*   By: lmangall <lmangall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:14:51 by lmangall          #+#    #+#             */
-/*   Updated: 2023/08/22 17:24:38 by lmangall         ###   ########.fr       */
+/*   Updated: 2023/08/24 15:53:54 by lmangall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,44 @@
 #include "../include/minishell.h"
 #include "../lib/libft/src/libft.h"
 
-// void retrieve_dollar()
+
+// static int replace_var(struct node_s *node)
+// {
+// 	ft_memcpy(node->str, get_vars_value(node->str), ft_strlen(get_vars_value(node->str)));
+// 	return (0);
+// }
+
+
+// struct node_s *contains_dollar(struct node_s *node)
+// {
+// 	if (node->type == NODE_COMMAND)
+// 	{
+// 		if (node->str[0] == '$')
+// 			return (node);
+// 	}
+// 	return (NULL);
+// }
+
+
+// void expansion(struct node_s *node, struct Vars *vars_container)
+// {
+// 	struct node_s *dollar_node;
+// 	dollar_node = contains_dollar(node);
+// 	if (dollar_node)
+// 	{
+// 		printf("dollar_node->str: %s\n", dollar_node->str);
+// 		while(node)
+// 		{
+// 			if (node->type == NODE_COMMAND)
+// 			{
+// 				if(dollar_node->str == vars_container->name)
+// 				{
+// 					replace_var(node);
+// 				}
+// 			}
+// 			node = node->next;
+// 		}
+
+		
+// 	}
+// }
