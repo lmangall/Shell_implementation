544db012e3b68723adffb62afdd03279ed2aa796
Léonard <lmangall@Leonards-MacBook-Pro.local>
2023-10-11 14:52:46 +0200

    Omio fixed pipe and cat | cat | ls

--- src/pipe.c
+++ src/pipe.c
@@ -45,7 +45,7 @@ void	first_child(struct node_s *node, int pipe_fd[2])
 	close(pipe_fd[1]);
 	exec_pipe_redir(node);
 }
-
+#if 0
 void	second_child(struct node_s *node, int pipe_fd[2])
 {
 	close(STDIN_FILENO);
@@ -54,7 +54,19 @@ void	second_child(struct node_s *node, int pipe_fd[2])
 	close(pipe_fd[1]);
 	exec_pipe_redir(node);
 }
+#endif
+void second_child(struct node_s *node, int pipe_fd[2])
+{
+    close(STDIN_FILENO);
+    dup(pipe_fd[0]);
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
+    exec_pipe_redir(node);
+    exit(EXIT_SUCCESS); // Beende den zweiten Kindprozess nach dem EOF
+}
 
+
+#if 0
 void	execute_pipe_command(struct node_s *node)
 {
 	pid_t child_pid;
@@ -83,4 +95,60 @@ void	execute_pipe_command(struct node_s *node)
 	close(pipe_fd[1]);
 	waitpid(child_pid, &status, 0);
 	// g_exit_status = status;
+}
+#endif
+void execute_pipe_command(struct node_s *node)
+{
+    pid_t child_pid1, child_pid2;
+    int pipe_fd[2];
+    int status;
+
+    node->operator = NONE;
+
+    if (pipe(pipe_fd) == -1)
+    {
+        // Fehlerbehandlung für pipe-Fehler hier einfügen
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    child_pid1 = fork();
+    if (child_pid1 == -1)
+    {
+        // Fehlerbehandlung für fork-Fehler hier einfügen
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child_pid1 == 0)
+    {
+        // Kindprozess 1
+        first_child(node, pipe_fd);
+    }
+    else
+    {
+        // Elternprozess
+        child_pid2 = fork();
+        if (child_pid2 == -1)
+        {
+            // Fehlerbehandlung für fork-Fehler hier einfügen
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (child_pid2 == 0)
+        {
+            // Kindprozess 2
+            second_child(node->next_sibling, pipe_fd);
+        }
+        else
+        {
+            // Elternprozess wartet auf beide Kinder
+            close(pipe_fd[0]); // Leseseite der Pipe wird im Elternprozess nicht benötigt
+            close(pipe_fd[1]); // Schreibseite der Pipe wird im Elternprozess nicht benötigt
+            waitpid(child_pid1, &status, 0);
+            waitpid(child_pid2, &status, 0);
+            // Fehlerbehandlung für waitpid-Fehler hier einfügen
+        }
+    }
 }
