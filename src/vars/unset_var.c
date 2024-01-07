9e2c6a3ebc0e7c3454127974467de7569499ddfd
Omio Horo <ohoro@c4b12c1.42berlin.de>
2024-01-07 11:45:33 +0100

    unset works now also with shell variables

--- src/vars/unset_var.c
+++ src/vars/unset_var.c
@@ -19,34 +19,50 @@
 #include <errno.h>
 #include <unistd.h>
 
-int	unset_shell_var(t_data *data, const char *name)
+int	find_var_index(t_data *data, const char *name)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	while (i < data->num_shell_vars)
 	{
 		if (ft_strcmp(data->shell_vc[i].name, name) == 0)
 		{
-			j = i;
-			while (j < data->num_shell_vars - 1)
-			{
-				ft_strlcpy(data->shell_vc[j].name,
-					data->shell_vc[j + 1].name, 50);
-				ft_strlcpy(data->shell_vc[j].value,
-					data->shell_vc[j + 1].value, 100);
-				j++;
-			}
-			data->num_shell_vars--;
-			data->shell_vc[data->num_shell_vars].name[0] = '\0';
-			data->shell_vc[data->num_shell_vars].value[0] = '\0';
-			return (0);
+			return (i);
 		}
 		i++;
 	}
-	perror("Variable not found");
 	return (-1);
+}
+
+void	remove_var_at_index(t_data *data, int index)
+{
+	int	j;
+
+	j = index;
+	while (j < data->num_shell_vars - 1)
+	{
+		ft_strlcpy(data->shell_vc[j].name, data->shell_vc[j + 1].name, 50);
+		ft_strlcpy(data->shell_vc[j].value, data->shell_vc[j + 1].value, 100);
+		j++;
+	}
+	data->num_shell_vars--;
+	data->shell_vc[data->num_shell_vars].name[0] = '\0';
+	data->shell_vc[data->num_shell_vars].value[0] = '\0';
+}
+
+int	unset_shell_var(t_data *data, const char *name)
+{
+	int	index;
+
+	index = find_var_index(data, name);
+	if (index == -1)
+	{
+		perror("Variable not found");
+		return (-1);
+	}
+	remove_var_at_index(data, index);
+	return (0);
 }
 
 int	unset_var(t_data *data, const char *name)
@@ -77,8 +93,9 @@ int	unset_var(t_data *data, const char *name)
 	return (-1);
 }
 
-// if the command "unset" is called with a space 
+// if the command "unset" is called with a space
 // and a variable name, it will call this function unset
+
 int	do_unset_builtin(char **tokens, t_data *data)
 {
 	int	i;
@@ -86,8 +103,15 @@ int	do_unset_builtin(char **tokens, t_data *data)
 	i = 1;
 	while (tokens[i] != NULL)
 	{
-		unset_var(data, tokens[i]);
-		i++;
+		if (unset_shell_var(data, tokens[i]) == 0)
+		{
+			i++;
+		}
+		else
+		{
+			unset_var(data, tokens[i]);
+			i++;
+		}
 	}
 	free_string_array(tokens);
 	return (-1);
