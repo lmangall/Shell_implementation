70560c61b031943eb2b8a208a3f8c17d6b131158
Omio Horo <ohoro@c4b4c1.42berlin.de>
2024-01-16 11:23:37 +0100

    env builtin norminetted

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: ohoro <ohoro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:06:14 by lmangall          #+#    #+#             */
-/*   Updated: 2024/01/15 10:53:25 by ohoro            ###   ########.fr       */
+/*   Updated: 2024/01/16 11:22:54 by ohoro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,71 +17,53 @@
 #include "../../lib/libft/src/libft.h"
 #include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define COLOR_RESET "\x1b[0m"
 #define COLOR_BOLD "\x1b[1m"
 #define COLOR_RED "\x1b[31m"
 #define COLOR_GREEN "\x1b[32m"
 
-int do_env_builtin(t_data *data)
+static void	print_variable(const char *name, const char *value)
 {
-    int i;
-    int j;
-
-    i = 0;
-    j = 0;
-
-    ft_putstr_fd("Name            Value\n", STDOUT_FILENO);
-
-    while (i < data->num_vars) {
-        ft_putstr_fd(data->vc[i].name, STDOUT_FILENO);
-        ft_putstr_fd("=", STDOUT_FILENO);
-        ft_putstr_fd(data->vc[i].value, STDOUT_FILENO);
-        ft_putstr_fd("\n", STDOUT_FILENO);
-        i++;
-    }
-
-    if (data->num_shell_vars > 0) {
-        ft_putstr_fd("Shell Vars (for clarity) :\n", STDOUT_FILENO);
-
-        while (j < data->num_shell_vars) {
-            ft_putstr_fd(data->shell_vc[j].name, STDOUT_FILENO);
-            ft_putstr_fd("=", STDOUT_FILENO);
-            ft_putstr_fd(data->shell_vc[j].value, STDOUT_FILENO);
-            ft_putstr_fd("\n", STDOUT_FILENO);
-            j++;
-        }
-    }
-
-    return -1;
+	write(STDOUT_FILENO, name, ft_strlen(name));
+	write(STDOUT_FILENO, "=", 1);
+	write(STDOUT_FILENO, value, ft_strlen(value));
+	write(STDOUT_FILENO, "\n", 1);
 }
 
-// this is the old beautiful version
-#if 0
-int	do_env_builtin(t_data *data)
+static void	print_environment_variables(t_data *data, int num_vars)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
-	printf(COLOR_BOLD "%-26s%-18s\n" COLOR_RESET, "Name", "Value");
-	while (i < data->num_vars)
+	while (i < num_vars)
 	{
-		printf("%s%-26s" COLOR_RESET, COLOR_GREEN, data->vc[i].name);
-		printf("%s\n", data->vc[i].value);
+		print_variable(data->vc[i].name, data->vc[i].value);
 		i++;
 	}
+}
+
+static void	print_shell_variables(t_data *data, int num_shell_vars)
+{
+	int	j;
+
+	j = 0;
+	write(STDOUT_FILENO, "Shell Vars (for clarity) :\n", 28);
+	while (j < num_shell_vars)
+	{
+		print_variable(data->shell_vc[j].name, data->shell_vc[j].value);
+		j++;
+	}
+}
+
+int	do_env_builtin(t_data *data)
+{
+	ft_putstr_fd("Name            Value\n", STDOUT_FILENO);
+	print_environment_variables(data, data->num_vars);
 	if (data->num_shell_vars > 0)
 	{
-		printf("%sShell Vars (for clarity) :\n" COLOR_RESET, COLOR_BOLD);
-		while (j < data->num_shell_vars)
-		{
-			printf("%s%-26s" COLOR_RESET, COLOR_RED, data->shell_vc[j].name);
-			printf("%s\n", data->shell_vc[j].value);
-			j++;
-		}
+		print_shell_variables(data, data->num_shell_vars);
 	}
 	return (-1);
 }
-#endif
