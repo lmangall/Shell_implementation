67627379c18b4d0ba617534880edcd1b93c5d110
Leonard Mangallon <lmangall@c4c6c1.42berlin.de>
2024-01-17 22:01:32 +0100

    last normi

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: lmangall <lmangall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 13:37:03 by ohoro             #+#    #+#             */
-/*   Updated: 2024/01/17 21:49:37 by lmangall         ###   ########.fr       */
+/*   Updated: 2024/01/17 22:00:56 by lmangall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -181,7 +181,7 @@ int			get_quote_type(char c);
 void		process_inside_quotes(char *new_str, size_t *j,
 				int *flag, char **original_str);
 void		handle_missing_variable(char *var_name, char *xpned_str, int *idx);
-void		process_variable(char *str, t_data *data, char *xpned_str, int *idx);
-
+void		process_variable(char *str, 
+				t_data *data, char *xpned_str, int *idx);
 
 #endif 
